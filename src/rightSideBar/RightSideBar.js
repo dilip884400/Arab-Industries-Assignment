2baa901912d2249f31b1360dbc4f0c229ade74ba
Dilip <dilip.884400@gmail.com>
2023-09-20 04:04:34 +0530

    assignment done

---
+++ src/rightSideBar/RightSideBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+function RightSideBar() {
+  return (
+      <div class="grid-item-1">
+          <h2>Right</h2>
+    </div>
+  )
+}
+
+export default RightSideBar
