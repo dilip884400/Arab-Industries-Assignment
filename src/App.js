36beb21eb64835ed988ad3396b1218e148cd7e74
Dilip <dilip.884400@gmail.com>
2023-09-20 04:32:26 +0530

    parallax assignment done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './header/Header'
-import LeftSideBar from './leftSideBar/LeftSideBar'
-import Home from './home/Home'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import TextBlock from './textBlock';
+import './App.css';
 
 function App() {
-  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
-
-  const OpenSidebar = () => {
-    setOpenSidebarToggle(!openSidebarToggle)
-  }
-
   return (
-    <div className='grid-container'>
-      <Header OpenSidebar={OpenSidebar} />
-      <LeftSideBar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar} />
-      <Home />
+    <div className="App">
+      <Parallax pages={2} style={{ top: '0', left: '0' }} class="animation">
+        <ParallaxLayer offset={0} speed={0.25}>
+          <div class="animation_layer parallax" id="artback"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.3}>
+          <div class="animation_layer parallax" id="mountain"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={-0.1}>
+          <div class="animation_layer parallax" id="logoland"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.3}>
+          <div class="animation_layer parallax" id="jungle1"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.35}>
+          <div class="animation_layer parallax" id="jungle2"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.5}>
+          <div class="animation_layer parallax" id="jungle3"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.45}>
+          <div class="animation_layer parallax" id="jungle4"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.40}>
+          <div class="animation_layer parallax" id="manonmountain"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.35}>
+          <div class="animation_layer parallax" id="jungle5"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.25}>
+          <TextBlock />
+        </ParallaxLayer>
+      </Parallax>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
